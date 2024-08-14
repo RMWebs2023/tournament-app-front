f287a9129b3facda67932f826cbd0c055b87ff14
CortezWalter <cortezwaltera@gmail.com>
2024-08-14 18:18:14 -0300

    Front
    Se instala SCSS
    Se instala Font Awesome
    Se instala react icons
    
    Se empieza con el front del login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <h1>Este es el home</h1>
+      <h1 className="home_h1">Este es el home</h1>
       <Link to="/login">
         <button>Haz click aquí y sabrás la verdad</button>
       </Link>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+
 
 const Login = () => {
   return (
     <>
-      <h1>Wally its gay</h1>
+      <div>Wally its gay</div>
+      <div>
+        <div className="login_background">
+          <div className="shape"></div>
+          <div className="shape"></div>
+        </div>
+      </div>
+      <form>
+        <h3>Login here</h3>
+
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username"></input>
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password"></input>
+
+        <button>Log In</button>
+        <div className="social">
+          <div className="login_go"><FaGoogle /> Google</div>
+          <div className="login_fb"><FaFacebookF /> Facebook</div>
+        </div>
+      </form>
     </>
   );
 };
